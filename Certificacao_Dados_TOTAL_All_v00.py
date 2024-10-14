7e23c263aa11808d9442e17c392ab052e4c264ad
João Vítor <joovitor177@gmail.com>
2024-10-14 15:50:07 -0300

    finalizando até o teste de persistencia

--- Certificacao_Dados_TOTAL_All_v00.py
+++ Certificacao_Dados_TOTAL_All_v00.py
@@ -44,107 +44,12 @@ dados = total_varrad(raw_rad, dia_juliano_ref, latitude, longitude, longitude_re
 from total_cqd_rad import total_cqd_rad
 
 # %%
-def total_cqd_rad2(raw_rad, raw_met, dados, ghi1, ghi2, ghi3, poa, gri1, gri2, dhi, bni, clear_sky, mes, dia_final, ano, nome_arquivo, es):
-    #data = raw_rad.iloc[:, 0].to_numpy()
-
-
-    start_row = 1440-20
-
-    ghi1_avg = raw_rad.iloc[:, ghi1[0]]
-    ghi1_max = raw_rad.iloc[:, ghi1[1]]
-    ghi1_min = raw_rad.iloc[:, ghi1[2]]
-    ghi1_std = raw_rad.iloc[:, ghi1[3]]
-    ghi1_avg_p = raw_rad.iloc[start_row:, ghi1[0]]
-
-    if len(ghi2) > 0:
-        ghi2_avg = raw_rad.iloc[:, ghi2[0]]
-        ghi2_max = raw_rad.iloc[:, ghi2[1]]
-        ghi2_min = raw_rad.iloc[:, ghi2[2]]
-        ghi2_std = raw_rad.iloc[:, ghi2[3]]
-        ghi2_avg_p = raw_rad.iloc[start_row:, ghi2[0]]
-
-    if len(ghi3) > 0:
-        ghi3_avg = raw_rad.iloc[:, ghi3[0]]
-        ghi3_max = raw_rad.iloc[:, ghi3[1]]
-        ghi3_min = raw_rad.iloc[:, ghi3[2]]
-        ghi3_std = raw_rad.iloc[:, ghi3[3]]
-        ghi3_avg_p = raw_rad.iloc[start_row:, ghi3[0]]
-
-    if len(poa) > 0:
-        poa_avg = raw_rad.iloc[:, poa[0]]
-        poa_max = raw_rad.iloc[:, poa[1]]
-        poa_min = raw_rad.iloc[:, poa[2]]
-        poa_std = raw_rad.iloc[:, poa[3]]
-        poa_avg_p = raw_rad.iloc[start_row:, poa[0]]
-
-    if len(dhi) > 0:
-        dhi_avg = raw_rad.iloc[:, dhi[0]]
-        dhi_max = raw_rad.iloc[:, dhi[1]]
-        dhi_min = raw_rad.iloc[:, dhi[2]]
-        dhi_std = raw_rad.iloc[:, dhi[3]]
-        dhi_avg_p = raw_rad.iloc[start_row:, dhi[0]]
-
-    if len(bni) > 0:
-        bni_avg = raw_rad.iloc[:, bni[0]]
-        bni_max = raw_rad.iloc[:, bni[1]]
-        bni_min = raw_rad.iloc[:, bni[2]]
-        bni_std = raw_rad.iloc[:, bni[3]]
-        bni_avg_p = raw_rad.iloc[start_row:, bni[0]]
-
-    if len(gri1) > 0:
-        gri1_avg = raw_rad.iloc[:, gri1[0]]
-        gri1_max = raw_rad.iloc[:, gri1[1]]
-        gri1_min = raw_rad.iloc[:, gri1[2]]
-        gri1_std = raw_rad.iloc[:, gri1[3]]
-        gri1_avg_p = raw_rad.iloc[start_row:, gri1[0]]
-
-    if len(gri2) > 0:
-        gri2_avg = raw_rad.iloc[:, gri2[0]]
-        gri2_max = raw_rad.iloc[:, gri2[1]]
-        gri2_min = raw_rad.iloc[:, gri2[2]]
-        gri2_std = raw_rad.iloc[:, gri2[3]]
-        gri2_avg_p = raw_rad.iloc[start_row:, gri2[0]]
-
-    if len(clear_sky) > 0:
-        clear_sky_avg = raw_rad.iloc[:, clear_sky[0]]
-        clear_sky_max = raw_rad.iloc[:, clear_sky[1]]
-        clear_sky_min = raw_rad.iloc[:, clear_sky[2]]
-        clear_sky_std = raw_rad.iloc[:, clear_sky[3]]
-        clear_sky_avg_p = raw_rad.iloc[start_row:, clear_sky[0]]
-
-    n, m = raw_rad.shape
-
-    horalocal = dados.iloc[:,1]
-    dia_mes = dados.iloc[:,2]
-    cosAZS = dados.iloc[:,13]
-    azs = dados.iloc[:,14]
-    cosAZS12 = dados.iloc[:,15]
-    alpha = dados.iloc[:,17]
-    ioh = dados.iloc[:,19]
-    iox = dados.iloc[:,21]
-
-# %return
-# if isempty(GHI2)    
-# [GHI_M1,GHI_N1,GHI_Nome,GHI_XLSX,Flags_GHI,Estatistico_GHI,Pot_GHI_xlsx,Energia_GHI_xlsx] = Sequencial_GHI(RAW_RAD,DADOS,GHI1_avg,GHI1_max,GHI1_min,GHI1_std,GHI1_avg_p,'Global Horizontal Irradiance ','GHI',GHI2,GHI3,POA,DHI,BNI,Clear_sky,mes,dia_final,ano,Nome_Arquivo);
-# [GHI_Over] = TOTAL_Over_Irradiance(RAW_RAD,DADOS,GHI1,Clear_sky,dia_final,mes,ano,'Overirradiance Events - GHI ','GHI',Nome_Arquivo);
-# end
-
-    # TO AQUI
-    if not ghi2:
-# [GHI_M1,GHI_N1,GHI_Nome,GHI_XLSX,Flags_GHI,Estatistico_GHI,Pot_GHI_xlsx,Energia_GHI_xlsx] = Sequencial_GHI(RAW_RAD,DADOS,GHI1_avg,GHI1_max,GHI1_min,GHI1_std,GHI1_avg_p,'Global Horizontal Irradiance ','GHI',GHI2,GHI3,POA,DHI,BNI,Clear_sky,mes,dia_final,ano,Nome_Arquivo);
-        ghi_m1, ghi_n1, ghi_nome, ghi_xlsx, flags_ghi, estatistico_ghi, pot_ghi_xlsx, energia_ghi_xlsx = sequencial_ghi(raw_rad, dados, ghi1_avg, ghi1_max, ghi1_min, ghi1_std, ghi1_avg_p, 'Global Horizontal Irradiance ', 'GHI', ghi2, ghi3, poa, dhi, bni, clear_sky, mes, dia_final, ano, nome_arquivo)
-
-
-    return ghi1_avg, ghi1_max, ghi1_avg_p
 # Controle de qualidade de Rados Radiométricos
 
 # [Estatistico_RAD,RAD_XLSXplot,Flags_RAD,Pot_RAD_xlsx,Energia_RAD_xlsx] = 
 # TOTAL_CQD_RAD(RAW_RAD,RAW_MET,DADOS,GHI1,GHI2,GHI3,POA,GRI1,GRI2,DHI,BNI,Clear_sky,mes,dia_final,ano,Nome_Arquivo,ES);
 #estatistico_rad, rad_xlsxplot, flags = total_cqd_rad(raw_rad, raw_met, dados, ghi1, ghi2, ghi3, poa, gri1, gri2, dhi, bni, clear_sky, mes, dia_final, ano, nome_arquivo, es)
-receba = total_cqd_rad2(raw_rad, raw_met, dados, ghi1, ghi2, ghi3, poa, gri1, gri2, dhi, bni, clear_sky, mes, dia_final, ano, nome_arquivo, es)
+total_cqd_rad(raw_rad, raw_met, dados, ghi1, ghi2, ghi3, poa, gri1, gri2, dhi, bni, clear_sky, mes, dia_final, ano, nome_arquivo, es)
 
 # %%
-type(c)
-# %%
-b
 # %%
--- sequencial_ghi.py
+++ sequencial_ghi.py
@@ -1,9 +1,23 @@
 import numpy as np
+import pandas as pd
+from testes.teste_limites_fisicos import teste_limites_fisicos
+from testes.teste_bsrn import teste_bsrn
+from testes.teste_angulo_elevacao import teste_angulo_elevacao
+from testes.teste_kt_ghi import teste_kt_ghi
+from testes.teste_std_consistencia import teste_std_consistencia
+from testes.teste_comparacao_simples import teste_comparacao_simples
+from testes.teste_comparacao_completo import teste_comparacao_completo
+from testes.teste_clear_sky import teste_clear_sky
+from testes.teste_consistencia import teste_consistencia
+from testes.teste_persistencia import teste_persistencia
 
-def sequencial_ghi(raw, dados, var_avg, var_max, var_min, var_std, ghi1_avg_p, titulo, nome_var, ghi2, ghi3, poa, dhi, bni, clear_sky, mes, dia_final, ano, nome_arquivo):
+
+def sequencial_ghi(raw, dados, var_avg, var_max, var_min, var_std, var_avg_p, titulo, nome_var, ghi2, ghi3, poa, dhi, bni, clear_sky, mes, dia_final, ano, nome_arquivo):
     n, m = raw.shape
 
     data = raw.iloc[:, 0].to_numpy()
+
+    clear_sky_ghi = 0
 
     if ghi2:
         ghi2_avg = raw.iloc[:, ghi2[0]]
@@ -16,7 +30,7 @@ def sequencial_ghi(raw, dados, var_avg, var_max, var_min, var_std, ghi1_avg_p, t
     if bni:
         bni_avg = raw.iloc[:, bni[0]]
     if clear_sky:
-        clear_sky_avg = raw.iloc[:, clear_sky[0]]
+        clear_sky_ghi = raw.iloc[:, clear_sky[0]]
 
     # Informações da varRad
     horalocal = dados.iloc[:,1]
@@ -56,7 +70,70 @@ def sequencial_ghi(raw, dados, var_avg, var_max, var_min, var_std, ghi1_avg_p, t
     n1[5] = ghi1_flag6
 
     # Nome
+    lf_ghi1, lf_ghi_flag = teste_limites_fisicos(var_avg, var_avg, 2000, -5, n)
+
+    # TESTE BSNR
+    fpmin = -4
+    fpmaxghi = (1.5 * iox * cosAZS12) + 100
+    ermin = -2
+    ermaxghi = (1.2 * iox * cosAZS12) + 50
+
+    #print(f"Teste aplicado Fisicamente Possível: lf_ghi1: {lf_ghi1} e {lf_ghi_flag}")
+    bsrn_ghi1, bsnr_ghi1_flag = teste_bsrn(lf_ghi1, var_avg, fpmin, fpmaxghi, ermin, ermaxghi, n)
+
+    #print(f"Teste aplicado BSNR: lf_ghi1: {bsrn_ghi1} e {bsnr_ghi1_flag}")
+
+    #print("AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII: ", type(bsrn_ghi1))
+
+    elevacao_ghi, elevacao_ghi_flag = teste_angulo_elevacao(bsrn_ghi1[:, 1], alpha, n)
+
+    #print(f"Teste aplicado Angulo: lf_ghi1: {elevacao_ghi} e {elevacao_ghi_flag}")
+
+    kt_ghi, kt_ghi1_flag = teste_kt_ghi(elevacao_ghi, var_avg, cosAZS, iox, n)
+
+    #print(f"Teste aplicado KT GHI: lf_ghi1: {kt_ghi} e {kt_ghi1_flag}")
+
+    # [std_Consistencia,std_Consistencia_flag] = TESTE_std_Consistencia(kt_GHI,Var_avg,Var_max,Var_min,Var_std,n);
+    std_consistencia, std_consistencia_flag = teste_std_consistencia(kt_ghi[:, 0], var_avg, var_max, var_min, var_std, n)
+    var_anterior = std_consistencia
+
+    #print(f"Teste aplicado STD Consistencia: lf_ghi1: {std_consistencia} e {std_consistencia_flag}")
+
+    if ghi2 and not ghi3:
+        aux = []
+        comparacao, comparacao_flag = teste_comparacao_simples(var_anterior, ghi2_avg, aux, n)
+        var_anterior = comparacao
+    if ghi2 and ghi3:
+        comparacao, comparacao_flag = teste_comparacao_simples(var_anterior, ghi2_avg, aux, n)
+        var_anterior = comparacao
+    if dhi:
+        comparacao_comp, comparacao_comp_flag  = teste_comparacao_completo(var_anterior[:,0], var_avg, dhi_avg, bni_avg, cosAZS, azs, clear_sky_ghi, n)
+        var_anterior = comparacao_comp
+        print(comparacao_comp_flag)
+
+    ghi_mcc_clearx = clear_sky_ghi*1.4
+    ceu_claro_ghi, ceu_claro_ghi_flag = teste_clear_sky(var_anterior, var_avg, ghi_mcc_clearx, n)
+    print("_--------------------------")
+    print(ceu_claro_ghi, ceu_claro_ghi_flag)
+
+    consistencia, consistencia_flag = teste_consistencia(ceu_claro_ghi, var_avg, n)
+
+    persistencia, persistencia_flag = teste_persistencia(consistencia, var_avg_p, 20, n)
+    
+# %==========================================================================
+# %                            GHI - Resultados
+# %==========================================================================
+
+# [Resultado_GHI1,Resultado_Flag_GHI1,Flags_GHI1,Estatistico_GHI1,GHI1_XLSX] = Resultado_Var(persistencia,Var_avg,Nome,nome_var,data,N1,n);
+# M1 = [M1,Resultado_GHI1];
+# N1 = [N1,Resultado_Flag_GHI1];
+# Nome = [Nome,{'Resultado'}];
+
+    #resultado_var(persistencia, var_avg, nome_arquivo, nome_var, data, n1, n)
+
+
+# function [M1,N1,Nome,GHI1_XLSX,Flags_GHI1,Estatistico_GHI1,Pot_GHI1_xlsx,Energia_GHI1_xlsx] = Sequencial_GHI(RAW,DADOS,Var_avg,Var_max,Var_min,Var_std,Var_avg_p,titulo,nome_var,GHI2,GHI3,POA,DHI,BNI,Clear_sky,mes,dia_final,ano,Nome_Arquivo)
 
 
 
-    pass
+    return 
