441fbd302542affcc8c8cb129701cfd68eecbfaf
João Vítor Fonseca de Mendonça <joovitor177@gmail.com>
2024-10-22 09:19:11 -0300

    Delete primos.py

--- primos.py
+++
@@ -1,9 +0,0 @@
-n = 200
-for j in range(1, n):
-    contador = 0
-    for i in range(1, j+1):  # Verifica divisores de 1 até j
-        if j % i == 0:
-            contador += 1
-
-    if contador == 2:  # Um número primo tem exatamente 2 divisores (1 e ele mesmo)
-        print(f"{j} é primo")
